870e85e8ecfb3e7b9a8799844ee0c879242ed68f
Koushik-Mallick <koushikmallick997@gmail.com>
2024-11-05 11:34:23 +0530

    Vergil1000

---
+++ src/components/Sakura_Background.tsx
@@ -0,0 +1,13 @@
+export default function Sakura_Background() {
+  return (
+    <iframe
+      src="https://vergil1000x.github.io/Sakura_Background/"
+      width="100%"
+      height="100%"
+      title="Sakura Background"
+      loading="lazy"
+      style={{ border: "none" }}
+      className="absolute -z-10"
+    />
+  );
+}
